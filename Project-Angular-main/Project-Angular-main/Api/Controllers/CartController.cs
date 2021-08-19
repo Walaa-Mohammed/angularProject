4193123f867f3b85bb75f8d4f3ddece08a0de989
Walaa Mohamed <wm205mohamed@gmail.com>
2021-08-19 15:22:39 +0200

    first commit

---
+++ Project-Angular-main/Project-Angular-main/Api/Controllers/CartController.cs
@@ -0,0 +1,82 @@
+﻿using BL.AppServices;
+using BL.Dtos;
+using DAL.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+        ProductCartAppService _productCartAppService;
+        ProductAppService _productAppService; 
+        PaymentAppService _paymentAppService; 
+        CartAppService _cartAppService;
+        IHttpContextAccessor _httpContextAccessor;
+
+        public CartController(ProductCartAppService productCartAppService,
+            ProductAppService productAppService ,
+            PaymentAppService paymentAppService ,
+            CartAppService cartAppService ,
+            IHttpContextAccessor httpContextAccessor)
+        {
+            this._productCartAppService = productCartAppService;
+            this._productAppService = productAppService;
+            this._paymentAppService = paymentAppService;
+            this._cartAppService = cartAppService;
+            this._httpContextAccessor = httpContextAccessor;
+        }
+        [HttpGet]
+        public ActionResult Index()
+        {
+
+            //get all products in specfic cart
+            //firs get cart id of logged user
+            var userID = _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+           
+            var productCartVM = _productCartAppService.GetAllProductCart(userID);
+           
+            return Ok(productCartVM);
+        }
+
+        [HttpPost("{productID}")]
+        [Authorize]
+        public IActionResult AddProductToCart(int productID)
+        {
+            //get cart of current logged user
+            var userID = _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            var productCart = new ProductCart() { CartID = userID, productId = productID };
+            var isExistingProductCartViewModel = _productCartAppService.CheckIfProductExistsInCart(userID, productID);
+            if (isExistingProductCartViewModel == false)
+            {
+                _productCartAppService.SaveNewProductCart(productCart);
+                return Ok();
+            }
+            return BadRequest("This product already exist in cart");
+
+        }
+
+        [HttpDelete("{productID}")]
+        public ActionResult DeleteProductFromCart(int productID)
+        {
+            var userID = _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+           
+            var isExistingProductCartViewModel = _productCartAppService.CheckIfProductExistsInCart(userID, productID);
+            if (isExistingProductCartViewModel == true)
+            {
+                _productCartAppService.DeleteProductCart(_productCartAppService.GetProductCartID(userID,productID));
+                return Ok();
+            }
+            return BadRequest("This product doesn't exist in cart");
+        }
+    }
+}
---
+++ Project-Angular-main/Project-Angular-main/Api/Controllers/WishlistController.cs
@@ -0,0 +1,80 @@
+﻿using BL.AppServices;
+using BL.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+   [Route("api/[controller]")]
+    [ApiController]
+    public class WishlistController : ControllerBase
+    {
+        ProductWishListAppService _productWishListAppService;
+        ProductAppService _productAppService;
+        WishlistAppService _wishlistAppService;
+        IHttpContextAccessor _httpContextAccessor;
+        public WishlistController(ProductWishListAppService productWishListAppService,
+                                  ProductAppService productAppService,
+                                  WishlistAppService wishlistAppService,
+                                  IHttpContextAccessor httpContextAccessor)
+
+        {
+            this._productAppService = productAppService;
+            this._wishlistAppService = wishlistAppService;
+            this._productWishListAppService = productWishListAppService;
+            this._httpContextAccessor = httpContextAccessor;
+        }
+        [HttpGet]
+        public IActionResult getUserWishList()
+        {
+            //get all products in specfic wishlist
+            //firs get cart id of logged user
+            var userID = _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+           
+            var productIDs = _productWishListAppService.GetAllProductWishList().Where(w => w.wishlistId == userID).Select(wpr => wpr.productId);
+            List<ProductViewModel> productViewModels = new List<ProductViewModel>();
+            foreach (var proID in productIDs)
+            {
+                var product = _productAppService.GetProduct(proID);
+                productViewModels.Add(product);
+            }
+            return Ok(productViewModels);
+        }
+       
+        [HttpPost("{productID}")]
+        public IActionResult AddProductToWishList(int productID)
+        {
+            //get wishlist of current logged user
+            var userID = _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+          
+            var productWishListViewModel = new ProductWishListViewModel() { wishlistId = userID, productId = productID };
+           
+            var isExistingProductWishListViewModel = _productWishListAppService.CheckIfProductExistsInWishlist(userID, productID);
+            if (isExistingProductWishListViewModel == false)
+            {
+                _productWishListAppService.SaveNewProductWishlist(productWishListViewModel);
+                return Ok();
+            }
+            return BadRequest("this product already exist in wishList");
+        }
+        [HttpDelete("{productID}")]
+        public IActionResult DeleteFromWishList(int productID)
+        {
+            var userID = _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            var isExistingProductWishListViewModel = _productWishListAppService.CheckIfProductExistsInWishlist(userID, productID);
+            if (isExistingProductWishListViewModel == true)
+            {
+                _productWishListAppService.DeleteProductWishList(_productWishListAppService.GetProductWishlistID(userID,productID));
+                return Ok();
+            }
+            return BadRequest("this product doesn't exist in wishList");
+           
+
+        }
+    }
+}
